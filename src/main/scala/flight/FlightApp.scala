e689211b52c387a4bf7587a7459cbb4a8c46869c
haghard <haghard84@gmail.com>
2024-08-08 23:21:01 -0400

    minor refactoring

--- src/main/scala/flight/FlightApp.scala
+++ src/main/scala/flight/FlightApp.scala
@@ -1,95 +1,105 @@
 package flight
 
 import scala.reflect.ClassTag
+import Airport.*
 
-object FlightApp {
+sealed trait Airport
 
-  sealed trait Airport
-
+object Airport {
   // abstract - you need a sub type in order to instantiate it
   // final - you can't subtype
   final abstract class NYK extends Airport
-
   final abstract class LAL extends Airport
-
   final abstract class DEN extends Airport
+}
 
-  sealed trait Itinerary[Src <: Airport, Dest <: Airport]
+sealed trait Itinerary[Src <: Airport, Dest <: Airport]
 
-  object Itinerary {
+object Itinerary {
 
-    final case class Flight[A <: Airport: ClassTag, B <: Airport: ClassTag](departureTs: Long, arrivalTs: Long)
-        extends Itinerary[A, B] {
-      override val toString: String =
-        s"${implicitly[ClassTag[A]].runtimeClass.getSimpleName}($departureTs) ~> ${implicitly[ClassTag[B]].runtimeClass.getSimpleName}($arrivalTs)"
+  final case class Flight[A <: Airport: ClassTag, B <: Airport: ClassTag](
+      departureTs: Long,
+      arrivalTs: Long)
+      extends Itinerary[A, B] {
+
+    override val toString: String = {
+      val from = implicitly[ClassTag[A]].runtimeClass.getSimpleName
+      val to = implicitly[ClassTag[B]].runtimeClass.getSimpleName
+      s"$from($departureTs) ~> $to($arrivalTs)"
     }
-
-    final case class Both[A <: Airport, B <: Airport, C <: Airport](left: Itinerary[A, B], right: Itinerary[B, C])
-        extends Itinerary[A, C]
-
-    implicit class ItineraryOps[A <: Airport, B <: Airport](val self: Itinerary[A, B]) extends AnyVal {
-
-      def ~>[C <: Airport](that: Itinerary[B, C]): Itinerary[A, C] =
-        Itinerary.Both(self, that)
-
-      def <~[C <: Airport](that: Itinerary[B, C])(implicit ev: A `IsEqualTo` C): Itinerary[A, C] =
-        Itinerary.Both(self, that)
-    }
-
-    def flight[From <: Airport: ClassTag, To <: Airport: ClassTag](
-        departureTs: Long,
-        arrivalTs: Long,
-      )(implicit ev: From `≠` To
-      ) =
-      Flight[From, To](departureTs, arrivalTs) // If From ≠ To, the ev is generated. Otherwise,
-
-    /*def match0[From <: Airport, To <: Airport](it: Itinerary[From, To]): Int =
-      it match {
-        case _: Flight[from, to] => 1
-        //case _                   => 0
-      }*/
-
-    def draw0(it: Itinerary[?, ?]): Unit =
-      it match {
-        case f @ Flight(_, _) =>
-          println(f.toString)
-        case Both(left, right) =>
-          draw0(left)
-          draw0(right)
-      }
-
-    def draw(it: Itinerary[?, ?]): Unit =
-      it match {
-        case f: Flight[from, to] =>
-          println(f.toString)
-        case b: Both[from, via, to] =>
-          implicitly[from `≠` to]
-          draw(b.left)
-          draw(b.right)
-      }
-
-    def drawFromNYK(it: Itinerary[NYK, ?]): Unit =
-      it match {
-        case f: Flight[from, to] =>
-          println(f.toString)
-        case b: Both[from, via, to] =>
-          implicitly[from `≠` to]
-          draw(b.left)
-          draw(b.right)
-      }
   }
+
+  final case class Both[A <: Airport, B <: Airport, C <: Airport](
+      left: Itinerary[A, B],
+      right: Itinerary[B, C])
+      extends Itinerary[A, C]
+
+  implicit class ItineraryOps[A <: Airport, B <: Airport](val self: Itinerary[A, B]) extends AnyVal {
+
+    def ~>[C <: Airport](that: Itinerary[B, C]): Itinerary[A, C] =
+      Itinerary.Both(self, that)
+
+    def <~[C <: Airport](that: Itinerary[B, C])(implicit ev: A `IsEqualTo` C): Itinerary[A, C] =
+      Itinerary.Both(self, that)
+  }
+
+  def flight[From <: Airport: ClassTag, To <: Airport: ClassTag](
+      departureTs: Long,
+      arrivalTs: Long,
+    )(implicit ev: From `≠` To
+    ) =
+    Flight[From, To](departureTs, arrivalTs) // If From ≠ To, the ev is generated. Otherwise,
+
+  /*def match0[From <: Airport, To <: Airport](it: Itinerary[From, To]): Int =
+    it match {
+      case _: Flight[from, to] => 1
+      //case _                   => 0
+    }*/
+
+  def draw0(it: Itinerary[?, ?]): Unit =
+    it match {
+      case f @ Flight(_, _) =>
+        println(f.toString)
+      case Both(left, right) =>
+        draw0(left)
+        draw0(right)
+    }
+
+  def draw(it: Itinerary[?, ?]): Unit =
+    it match {
+      case f: Flight[from, to] =>
+        println(f.toString)
+      case b: Both[from, via, to] =>
+        implicitly[from `≠` to]
+        draw(b.left)
+        draw(b.right)
+    }
+
+  def drawFromNYK(it: Itinerary[NYK, ?]): Unit =
+    it match {
+      case f: Flight[from, to] =>
+        println(f.toString)
+      case b: Both[from, via, to] =>
+        implicitly[from `≠` to]
+        draw(b.left)
+        draw(b.right)
+    }
+}
+
+object FlightApp {
+  import Airport.*
 
   def main(args: Array[String]): Unit = {
 
     import Itinerary.*
 
     val a = flight[NYK, DEN](1, 2) ~> flight[DEN, LAL](3, 5)
-
     draw(a)
 
     // drawFromNYK(flight[DEN, NYK](1, 2))
 
-    flight[NYK, DEN](1, 2) <~ flight[DEN, NYK](3, 5)
+    val b = flight[NYK, DEN](1, 2) <~ flight[DEN, NYK](3, 5)
+    draw(b)
 
     // constraint violation: Actual flight.FlightApp.LAL but expected flight.FlightApp.NYK
     // flight[NYK, DEN](1, 2) <~ flight[DEN, LAL](3, 5)
--- src/main/scala/flight/FlightApp2.scala
+++ src/main/scala/flight/FlightApp2.scala
@@ -1,69 +1,69 @@
 package flight
 
-object FlightApp2 {
+sealed trait Airport2 {
+  def id: Long
+}
 
-  sealed trait Airport {
-    def id: Long
+object Airport2 {
+  abstract class AbsAirport(val id: Long) extends Airport2
+
+  final case class CHI() extends AbsAirport(1)
+
+  final case class ATL() extends AbsAirport(2)
+
+  final case class NYK() extends AbsAirport(3)
+
+  final case class DAL() extends AbsAirport(4)
+
+  final case class CHA() extends AbsAirport(5)
+
+  /*def fromString(line: String): Option[Airport] =
+    line.trim match {
+      case "1" => Some(CHI())
+      case "2" => Some(ATL())
+      case "3" => Some(NYK())
+      case _   => None
+    }*/
+}
+
+sealed trait Itinerary2[Origin <: Airport2, Dest <: Airport2]
+
+object Itinerary2 {
+
+  final case class Flight[A <: Airport2, B <: Airport2](from: A, to: B) extends Itinerary2[A, B]
+
+  final case class Both[A <: Airport2, B <: Airport2, C <: Airport2](
+      leftLeg: Itinerary2[A, B],
+      rightLeg: Itinerary2[B, C])
+      extends Itinerary2[A, C]
+
+  implicit class ItineraryOps[A <: Airport2, B <: Airport2](private val self: Itinerary2[A, B]) extends AnyVal {
+    def ~>[C <: Airport2](that: Itinerary2[B, C]): Itinerary2[A, C] =
+      Itinerary2.Both(self, that)
+
+    def <~[C <: Airport2](that: Itinerary2[B, C])(implicit ev: A `IsEqualTo` C): Itinerary2[A, C] =
+      Itinerary2.Both(self, that)
   }
 
-  object Airport {
-    abstract class AbsAirport(val id: Long) extends Airport
+  def flight[From <: Airport2, To <: Airport2](from: From, to: To)(implicit ev: From ≠ To) =
+    Flight[From, To](from, to)
 
-    final case class CHI() extends AbsAirport(1)
-
-    final case class ATL() extends AbsAirport(2)
-
-    final case class NYK() extends AbsAirport(3)
-
-    final case class DAL() extends AbsAirport(4)
-
-    final case class CHA() extends AbsAirport(5)
-
-    /*def fromString(line: String): Option[Airport] =
-      line.trim match {
-        case "1" => Some(CHI())
-        case "2" => Some(ATL())
-        case "3" => Some(NYK())
-        case _   => None
-      }*/
-  }
-
-  sealed trait Itinerary[Origin <: Airport, Dest <: Airport]
-
-  object Itinerary {
-
-    final case class Flight[A <: Airport, B <: Airport](from: A, to: B) extends Itinerary[A, B]
-
-    final case class Both[A <: Airport, B <: Airport, C <: Airport](
-        leftLeg: Itinerary[A, B],
-        rightLeg: Itinerary[B, C])
-        extends Itinerary[A, C]
-
-    implicit class ItineraryOps[A <: Airport, B <: Airport](private val self: Itinerary[A, B]) extends AnyVal {
-      def ~>[C <: Airport](that: Itinerary[B, C]): Itinerary[A, C] =
-        Itinerary.Both(self, that)
-
-      def <~[C <: Airport](that: Itinerary[B, C])(implicit ev: A `IsEqualTo` C): Itinerary[A, C] =
-        Itinerary.Both(self, that)
+  def draw(it: Itinerary2[?, ?]): Unit =
+    it match {
+      case Flight(from, to) =>
+        println(s"$from ~> $to")
+      case Both(left, right) =>
+        draw(left)
+        draw(right)
     }
+}
 
-    def flight[From <: Airport, To <: Airport](from: From, to: To)(implicit ev: From ≠ To) =
-      Flight[From, To](from, to)
-
-    def draw(it: Itinerary[?, ?]): Unit =
-      it match {
-        case Flight(from, to) =>
-          println(s"$from ~> $to")
-        case Both(left, right) =>
-          draw(left)
-          draw(right)
-      }
-  }
+object FlightApp2 {
 
   def main(args: Array[String]): Unit = {
 
-    import Airport.*
-    import Itinerary.*
+    import Airport2.*
+    import Itinerary2.*
 
     val a = flight(CHI(), ATL()) ~> flight(ATL(), NYK()) ~> flight(NYK(), DAL()) ~> flight(DAL(), CHA())
     draw(a)
